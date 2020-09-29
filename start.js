72db20a14ac92076b1a8cb092448ce56a6f677b3
matt-sanpedro <mattsanpedro76@gmail.com>
2020-09-28 22:15:10 -0700

    completed string exercises

---
+++ start.js
@@ -0,0 +1,13 @@
+// var message = prompt("Input message:");
+// // console.log(message);
+
+// var maxChar = 140;
+// var charLeft = maxChar - message.length;
+
+// alert("You have written " + message.length + " characters, you have " + charLeft + " characters left.");
+// alert(message.slice(0,maxChar));
+
+var name = 'matThEw';
+// console.log(name.toUpperCase().slice(0,1) + name.slice(1,name.length));
+
+console.log("Hello, " + name.toUpperCase().slice(0,1) + name.toLowerCase().slice(1,name.length));
