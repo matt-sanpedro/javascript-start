8c29d1e0c550372de7771720fdb20aa73b2d847c
matt-sanpedro <mattsanpedro76@gmail.com>
2020-09-28 22:40:29 -0700

    completed numbers exercise

---
+++ numbers-intro.js
@@ -0,0 +1,27 @@
+dogAge = 7;
+humanAge = ((dogAge - 2) * 4) + 21;
+// alert("Your dog is " + humanAge + " years old in human years!")
+
+var x;
+var y;
+
+// increment
+x = x + 1;
+x++;
+
+x = x + 2;
+x+= 2;
+
+// decrement
+x = x - 1;
+x--;
+
+x = x - 3;
+x-= 2;
+
+// exercise
+x = 1;
+y = 2;
+y = x++;
+y++;
+console.log(x, y)
